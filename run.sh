ee72a02ff0c4461de4e4e71d57e14f780c536011
Jay Shridharani <jshridha@gmail.com>
2016-12-21 19:20:39 -0500

    allow ability to set outside url

--- run.sh
+++ run.sh
@@ -4,4 +4,13 @@ set -e
 
 if [ $DEVPI_DEBUG = "true" ]; then export DEBUG=--debug; echo "Debugging server"; fi
 
-devpi-server $DEBUG --host 0.0.0.0 --serverdir $DEVPI_SERVERDIR --port $DEVPI_SERVERPORT
+STARTUP_COMMAND="$DEBUG --host 0.0.0.0 --serverdir $DEVPI_SERVERDIR --port $DEVPI_SERVERPORT"
+
+if [ -z ${DEVPI_OUTSIDE+x} ]; then
+  echo NOT SETTING OUTSIDE URL
+else
+  echo "OUTSIDE URL SET"
+  STARTUP_COMMAND="$STARTUP_COMMAND --outside-url $DEVPI_OUTSIDE"
+fi
+
+devpi-server $STARTUP_COMMAND
